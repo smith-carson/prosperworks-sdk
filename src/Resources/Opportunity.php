ed3a781b1a18c39fd5d5decbc81987a74446ff91
Leonardo Pedretti <lpedretti@codigodinamo.com>
2017-10-11 19:49:46 -0300

    Added opportunity resource

---
+++ src/Resources/Opportunity.php
@@ -0,0 +1,16 @@
+<?php
+namespace ProsperWorks\Resources;
+
+use ProsperWorks\SubResources\Address;
+use ProsperWorks\SubResources\CustomField;
+use ProsperWorks\SubResources\Phone;
+use ProsperWorks\SubResources\URL;
+
+/**
+ * Opportunity resource object.
+ * @author lpedretti
+ */
+class Opportunity extends BareResource
+{
+}
+
