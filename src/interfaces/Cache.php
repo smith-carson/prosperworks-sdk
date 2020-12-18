cbb7bf4b44d8533eb6a066aa87d998b9bc88a576
Vladimir Chetalbashev <VladimirChetalbashev@Jiminnys-MacBook-Pro.local>
2020-12-18 10:26:25 +0200

    push branch minor changes

--- src/interfaces/Cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ProsperWorks\Interfaces;
-
-interface Cache
-{
-    public function get($key):mixed;
-    public function save(string $key, mixed $data, int $lifetime = null);
-}
--- src/interfaces/Crypt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ProsperWorks\Interfaces;
-
-interface Crypt
-{
-    public function encryptBase64(string $string): string;
-    public function decryptBase64(string $string): string;
-}
