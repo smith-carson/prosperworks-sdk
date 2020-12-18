3f1561c73d45400c38fd871869d63f69241a7f3b
Vladimir Chetalbashev <VladimirChetalbashev@Jiminnys-MacBook-Pro.local>
2020-12-18 10:29:01 +0200

    add interfaces

---
+++ src/Interfaces/Cache.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProsperWorks\Interfaces;
+
+interface Cache
+{
+    public function get($key):mixed;
+    public function save(string $key, mixed $data, int $lifetime = null);
+}
---
+++ src/Interfaces/Crypt.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProsperWorks\Interfaces;
+
+interface Crypt
+{
+    public function encryptBase64(string $string): string;
+    public function decryptBase64(string $string): string;
+}
